1ff4e50ec15c1d6b1fb849a48d0b99b8aafb4190
cnetakaline <cnetakaline@gmail.com>
2024-05-19 07:12:06 -0400

    add myanmar font and setup file

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+sudo apt update
+sudo apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+sudo touch /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+command -v nvm
+nvm install --lts
+nvm use --lts
+npm install -g express mongoose dotenv apollo-server-express
+sudo apt install libu2f-udev -y
+sudo dpkg -i ./*.deb
+sudo mv ./font/*.ttf /usr/share/fonts
+reboot
